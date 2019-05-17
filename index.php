c9dfa6dad2b7f363930f744e1f94e578d59cc256
Ygor Amaral <ygor.amaral@ufrpe.br>
2019-05-17 18:14:43 -0300

    estou adicionando o arquivo index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once("pessoa.php");
+
+$pessoa = new Pessoa("Ygor", "123456");
+
+$pessoa->setNome("Ygor Amaral")->setCPF("dsadasdsa");
+
+$pessoa->setNome("Ygor Amaral");
+$pessoa->setCPF("Ygor Amaral");
+
+echo $pessoa->getNome();
+
+echo "<pre>";
+var_dump($pessoa);
+echo "</pre>";
